618d5f26f30bfd77b11132f076bc1001b22e8873
SirinRain <SirinRain55@gmail.com>
2025-06-11 09:43:10 +0600

    folder 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float MoveSpeed = 1f;
-    public Rigidbody2D rigidbody2D;
     public float impulse;
-    int applesCount;
+    float applesCount;
+    public Rigidbody2D rigidbody;
+    public float minimalHeight;
+    public bool isCheatMode;
+    public SpriteRenderer[] SpriteRenderer;
 
     private void Start()
     {
-        Debug.Log(applesCount);
+       
     }
+
     void Update()
     {
-        
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector2.left * Time.deltaTime * MoveSpeed);
@@ -26,7 +30,23 @@ public class Player : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidbody2D.AddForce(Vector2.up * impulse, ForceMode2D.Impulse);
+            rigidbody.AddForce(Vector2.up * impulse, ForceMode2D.Impulse);
         }
+
+        if (transform.position.y < minimalHeight && isCheatMode)
+        {
+            transform.position = new Vector2(0, 0);
+            rigidbody.velocity = new Vector2(0, 0);
+        }
+        else if (transform.position.y < minimalHeight)
+        { 
+            Destroy(gameObject);
+            
+        }
+        
+
+
+
+
     }
 }
